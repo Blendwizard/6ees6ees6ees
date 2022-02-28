a3213fae91322946ab55f914aed2003808b5e4f6
JoeVanCamp <vancampjoe@gmail.com>
2022-02-28 11:00:42 -0800

    Complete Grub super class and Bee subclass

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,9 @@
-class Bee {
-  // TODO..
+// Create Bee subclass
+class Bee extends Grub {
+  constructor () {
+    super(Grub);
+    this.age = 5;
+    this.color = 'yellow';
+    this.job = 'Keep on growing';
+  }
 };
--- src/Grub.js
+++ src/Grub.js
@@ -1,2 +1,14 @@
+// Create Grub super class
 class Grub {
+  // Create constructor
+  constructor () {
+    this.age = 0;
+    this.color = 'pink';
+    this.food = 'jelly';
+  }
+
+  // Add methods below for Grub
+  eat () {
+    return 'Mmmmmmmmm jelly';
+  }
 };
